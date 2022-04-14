fa25fca81716b4d48b1ee1e720dd94db69431839
Cristian David Herrera <dh99583@gmail.com>
2022-04-14 17:44:38 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+document.querySelector(".boton").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.sub-header');
+  ScrollReveal().reveal('.art2', { delay: 500 });
+  ScrollReveal().reveal('.art3', { delay: 500 });
+  ScrollReveal().reveal('.art4', { delay: 500 });
+  ScrollReveal().reveal('.art5', { delay: 500 });
+  ScrollReveal().reveal('.art6', { delay: 500 });
+  ScrollReveal().reveal('.art7', { delay: 500 });
+  ScrollReveal().reveal('.input', { delay: 500 });
